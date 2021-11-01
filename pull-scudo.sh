c9e8a0e2fda0ddb55ef5e02ae55f290b03b76851
Casper Neo <cneo@google.com>
2021-11-01 17:53:57 +0000

    Move scudo-standalone to third-party and add LLVM LICENSE.
    Moved:
      scudo-sys/scudo-standalone/* -> third_party/llvm-scudo-standalone/*
      scudo-sys/pull-scudo.sh -> pull-scudo.sh
    
    Added:
      third_party/llvm-scudo/standalone/LICENSE (copied from llvm's page)
    
    Manually verified:
      Cargo package --list confirms that packaging works
      as expected with a symlink.
    
    Change-Id: Ifbefd0b4d12e3465f5a344c63976fd60c87b0042

---
+++ pull-scudo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# This script will pull Scudo standalone files from the llvm project and update
+# the standalone directory. This is preferred over git submodule and git subtree
+# because those tools do not support mirroring a single directory and LLVM is a
+# large project.
+#
+# This script is expected to be run from the root of the `rust-scudo` project.
+#
+# Usage: pull-scudo.sh
+
+version="llvmorg-13.0.0"  # Keep me up to date!
+
+tmp_repo=$(mktemp -d)
+tmp_license=$(mktemp)
+scudo_standalone=third_party/llvm-scudo-standalone
+git clone --branch $version --depth 1 https://github.com/llvm/llvm-project.git $tmp_repo
+mv $scudo_standalone/LICENSE $tmp_license
+rm -rf $scudo_standalone
+mv $tmp_repo/compiler-rt/lib/scudo/standalone $scudo_standalone
+mv $tmp_license $scudo_standalone/LICENSE
+rm -rf $tmp_repo
+
